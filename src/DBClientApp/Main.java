0475fa21fb8a94fe00f65f23d6df90a415e953c3
icodeunot <jsal.dev@protonmail.com>
2023-07-17 22:19:47 -0700

    Initial commit

---
+++ src/DBClientApp/Main.java
@@ -0,0 +1,11 @@
+package DBClientApp;
+
+import helper.JDBC;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
+}
